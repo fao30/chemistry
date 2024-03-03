36c9ef0ecaee31dd30e491fa93023f2ca909718c
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-03-03 17:00:00 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import { getLang, setLang } from "./utils/functions";
 
 export default function App() {
+  useEffect(() => {
+    const lang = getLang();
+    if (!lang) setLang("ru");
+  }, []);
+
   return (
     <BrowserRouter>
       <AppRoutes />
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+type Lang = "en" | "ru";
+
 export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
+export const getLang = (): Lang | null => (localStorage.getItem("lang") as Lang) ?? null;
+export const setLang = (lang: Lang) => localStorage.setItem("lang", lang);
